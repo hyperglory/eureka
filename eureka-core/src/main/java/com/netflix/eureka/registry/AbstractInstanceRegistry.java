988571d358d47dbc7cf9c3af2c3dfa71af9c4890
Sagero <sagero@foxmail.com>
2018-06-23 15:56:17 +0800

    Eureka Server端是如何完成服务注册的

--- eureka-core/src/main/java/com/netflix/eureka/registry/AbstractInstanceRegistry.java
+++ eureka-core/src/main/java/com/netflix/eureka/registry/AbstractInstanceRegistry.java
@@ -194,6 +194,12 @@ public abstract class AbstractInstanceRegistry implements InstanceRegistry {
             read.lock();
             Map<String, Lease<InstanceInfo>> gMap = registry.get(registrant.getAppName());
             REGISTER.increment(isReplication);
+
+            /*
+              如果说是某个服务第一次来注册，很明显，通过AppName是获取不到Map的，是null
+              此时就会创建一个新的Map，放到大的registry map中去
+              其实这个registry map就是一个注册表，里面包含了每个服务实例的注册信息
+             */
             if (gMap == null) {
                 final ConcurrentHashMap<String, Lease<InstanceInfo>> gNewMap = new ConcurrentHashMap<String, Lease<InstanceInfo>>();
                 gMap = registry.putIfAbsent(registrant.getAppName(), gNewMap);
@@ -201,6 +207,12 @@ public abstract class AbstractInstanceRegistry implements InstanceRegistry {
                     gMap = gNewMap;
                 }
             }
+
+            /*
+              通过instanceId，从gMap中获取服务实例对应的租约
+              我们假设，是这个服务的这个服务实例，第一次来注册，那么这里获取到的Lease一定是null
+              因为这个服务实例之前没有来注册过
+             */
             Lease<InstanceInfo> existingLease = gMap.get(registrant.getId());
             // Retain the last dirty timestamp without overwriting it, if there is already a lease
             if (existingLease != null && (existingLease.getHolder() != null)) {
@@ -230,16 +242,22 @@ public abstract class AbstractInstanceRegistry implements InstanceRegistry {
                 }
                 logger.debug("No previous lease information found; it is new registration");
             }
+
+            // 在这里，会将这个InstanceInfo封装为一个Lease对象
             Lease<InstanceInfo> lease = new Lease<InstanceInfo>(registrant, leaseDuration);
             if (existingLease != null) {
                 lease.setServiceUpTimestamp(existingLease.getServiceUpTimestamp());
             }
+            // 直接将封装了服务实例信息的Lease对象，放到了gMap里去，key就是服务实例id
             gMap.put(registrant.getId(), lease);
+
+            // 最近注册的队列，保存注册服务的队列
             synchronized (recentRegisteredQueue) {
                 recentRegisteredQueue.add(new Pair<Long, String>(
                         System.currentTimeMillis(),
                         registrant.getAppName() + "(" + registrant.getId() + ")"));
             }
+            // 服务实例的状态
             // This is where the initial state transfer of overridden status happens
             if (!InstanceStatus.UNKNOWN.equals(registrant.getOverriddenStatus())) {
                 logger.debug("Found overridden status {} for instance {}. Checking to see if needs to be add to the "
